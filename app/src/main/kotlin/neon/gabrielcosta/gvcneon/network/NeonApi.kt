2b8cf87fae6f8c282a12b9a903a12b771e7a6c56
Gabriel Costa <gabrielcosta08@gmail.com>
2018-04-27 01:00:08 -0300

    Create a API for Neon Services

---
+++ app/src/main/kotlin/neon/gabrielcosta/gvcneon/network/NeonApi.kt
@@ -0,0 +1,21 @@
+package neon.gabrielcosta.gvcneon.network
+
+import android.arch.lifecycle.LiveData
+import neon.gabrielcosta.gvcneon.entity.dto.SendMoneyDTO
+import neon.gabrielcosta.gvcneon.entity.dto.TransferResponseDTO
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface NeonApi {
+
+    @GET("/GenerateToken")
+    fun generateToken(@Query("name") name: String,
+        @Query("String") email: String): LiveData<ApiResponse<String>>
+
+    @POST("/SendMoney")
+    fun sendMoney(request: SendMoneyDTO): LiveData<ApiResponse<Boolean>>
+
+    @GET("/GetTransfers")
+    fun getTransfers(@Query("token") token: String): LiveData<ApiResponse<TransferResponseDTO>>
+}
