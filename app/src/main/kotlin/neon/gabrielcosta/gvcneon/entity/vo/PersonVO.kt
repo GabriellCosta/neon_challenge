3ce009e07dd494fc1948435b75480ca1789dc236
Gabriel <gabrielcosta08@gmail.com>
2018-04-30 16:46:07 -0300

    Implement Parcelable on PersonVO

--- app/src/main/kotlin/neon/gabrielcosta/gvcneon/entity/vo/PersonVO.kt
+++ app/src/main/kotlin/neon/gabrielcosta/gvcneon/entity/vo/PersonVO.kt
@@ -1,5 +1,7 @@
 package neon.gabrielcosta.gvcneon.entity.vo
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 class PersonDTO(@SerializedName("results") val results: List<PersonVO>)
@@ -7,4 +9,32 @@ class PersonDTO(@SerializedName("results") val results: List<PersonVO>)
 data class PersonVO(@SerializedName("name") val name: String,
     @SerializedName("email") val email: String,
     @SerializedName("phone") val phone: String,
-    @SerializedName("image") val photo: String)
+    @SerializedName("image") val photo: String) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(email)
+        parcel.writeString(phone)
+        parcel.writeString(photo)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<PersonVO> {
+        override fun createFromParcel(parcel: Parcel): PersonVO {
+            return PersonVO(parcel)
+        }
+
+        override fun newArray(size: Int): Array<PersonVO?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
