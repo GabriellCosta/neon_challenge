f2657d5e0b87826584a4e3e3b085321d72487eef
Gabriel Costa <gabrielcosta08@gmail.com>
2018-04-30 11:09:10 -0300

    Change color of ItemDivider on PeopleList

--- app/src/main/kotlin/neon/gabrielcosta/gvcneon/ui/PeopleListActivity.kt
+++ app/src/main/kotlin/neon/gabrielcosta/gvcneon/ui/PeopleListActivity.kt
@@ -3,6 +3,8 @@ package neon.gabrielcosta.gvcneon.ui
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import neon.gabrielcosta.gvcneon.R
@@ -31,6 +33,14 @@ class PeopleListActivity : BaseActivity() {
     private fun initList(personVo: List<PersonVO>) {
         val recyclerView = findViewById<RecyclerView>(R.id.rv_people)
         recyclerView.adapter = PeopleListAdapter(personVo)
-        recyclerView.layoutManager = LinearLayoutManager(baseContext)
+        val linearLayoutManager = LinearLayoutManager(baseContext)
+        recyclerView.layoutManager = linearLayoutManager
+        val dividerItemDecoration = DividerItemDecoration(baseContext,
+            linearLayoutManager.orientation)
+        ContextCompat.getDrawable(baseContext,
+            R.drawable.item_decoration_divider)?.let {
+            dividerItemDecoration.setDrawable(it)
+        }
+        recyclerView.addItemDecoration(dividerItemDecoration)
     }
 }
