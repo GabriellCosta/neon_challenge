13f3cc567ecf7a424c598c37f5949c645b455296
Gabriel Costa <gabrielcosta08@gmail.com>
2018-04-30 02:56:04 -0300

    Create Activity to list people

--- app/src/main/kotlin/neon/gabrielcosta/gvcneon/dagger/ActivityModule.kt
+++ app/src/main/kotlin/neon/gabrielcosta/gvcneon/dagger/ActivityModule.kt
@@ -3,10 +3,14 @@ package neon.gabrielcosta.gvcneon.dagger
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import neon.gabrielcosta.gvcneon.ui.MainActivity
+import neon.gabrielcosta.gvcneon.ui.PeopleListActivity
 
 @Module
 abstract class ActivityModule {
 
     @ContributesAndroidInjector
     internal abstract fun contribute_MainActivity(): MainActivity
+
+    @ContributesAndroidInjector
+    internal abstract fun contribute_PeopleListActivity(): PeopleListActivity
 }
---
+++ app/src/main/kotlin/neon/gabrielcosta/gvcneon/ui/PeopleListActivity.kt
@@ -0,0 +1,36 @@
+package neon.gabrielcosta.gvcneon.ui
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import neon.gabrielcosta.gvcneon.R
+import neon.gabrielcosta.gvcneon.entity.vo.PersonVO
+import neon.gabrielcosta.gvcneon.vm.PeopleViewModel
+
+class PeopleListActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_people)
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        ViewModelProviders.of(this, factory)
+            .get(PeopleViewModel::class.java)
+            .fetchPeople()
+            .observe(this, Observer {
+                if (it!!.body != null) {
+                    initList(it.body?.results!!)
+                }
+            })
+    }
+
+    private fun initList(personVo: List<PersonVO>) {
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_people)
+        recyclerView.adapter = PeopleListAdapter(personVo)
+        recyclerView.layoutManager = LinearLayoutManager(baseContext)
+    }
+}
