5f3aff530a6be846c018ecc97ea6e7ed659605dc
Gabriel <gabrielcosta08@gmail.com>
2018-04-30 16:46:32 -0300

    Add fragment injector on BaseActivity

--- app/src/main/kotlin/neon/gabrielcosta/gvcneon/ui/BaseActivity.kt
+++ app/src/main/kotlin/neon/gabrielcosta/gvcneon/ui/BaseActivity.kt
@@ -1,12 +1,23 @@
 package neon.gabrielcosta.gvcneon.ui
 
 import android.arch.lifecycle.ViewModelProvider
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.HasSupportFragmentInjector
 import javax.inject.Inject
 
-abstract class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
     @Inject
     protected lateinit var factory: ViewModelProvider.Factory
+
+    @Inject
+    lateinit var activityInjector: DispatchingAndroidInjector<Fragment>
+
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> {
+        return activityInjector
+    }
 
 }
